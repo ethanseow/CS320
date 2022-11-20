84debd1f4c8fd67e5341406caffd77818419a00b
Ethan Seow <eseow6088@bths.edu>
2022-11-19 20:51:20 -0500

    saving changes here

--- interpreter.ml
+++ interpreter.ml
@@ -13,10 +13,11 @@
    related problem. *)
 
 (*NOTE: There are no restrictions on what you can use*)
-(*#load "str.cma";;*)
+#load "str.cma";;
 type const = 
   | Int of int
   | String of string
+  | Var of string
 type com = 
   | Quit
   | Push of const
@@ -28,9 +29,14 @@ type com =
   | Swap
   | Neg
   | Concat
+  | Begin
+  | End
+  | Global of const
+  | Local of const
 
 let regexp = Str.regexp "[-0-9]+$"
-let regexp2 = Str.regexp "[\\\"A-Za-z]+$"
+let regexp2 = Str.regexp "\\\"[A-Za-z]+\\\"$"
+let regexp3 = Str.regexp "[a-z]+$"
 let stringToConst (str : string ) : const =
   if Str.string_match regexp str 0
     then
@@ -42,7 +48,11 @@ let stringToConst (str : string ) : const =
       then
         String(str)
     else
-      String("error")
+      if Str.string_match regexp3 str 0
+        then
+          Var(str)
+      else
+        String("error")
 
 let stringToCom (inp : string list) : com =
   let comm = List.nth inp 0
@@ -53,6 +63,14 @@ let stringToCom (inp : string list) : com =
     let v = stringToConst (List.nth inp 1)
     in
     Push(v)
+  | "Global" -> 
+    let v = stringToConst (List.nth inp 1)
+    in
+    Global(v)
+  | "Local" -> 
+    let v = stringToConst(List.nth inp 1)
+    in
+    Local(v)
   | "Pop" -> Pop
   | "Add" -> Add
   | "Concat" -> Concat
@@ -62,6 +80,13 @@ let stringToCom (inp : string list) : com =
   | "Swap" -> Swap
   | "Neg" -> Neg
   | "Concat" -> Concat
+
+let printToFile (inp : string list) (file_path : string) : unit = 
+  let fp = open_out file_path in
+  let lekunga = (String.concat "\n" inp)
+  in
+  let () = Printf.fprintf fp "%s" lekunga in
+    close_out fp
 
 let srcToCom (src : string) : com list = 
   let cmds = String.split_on_char '\n' src
@@ -80,17 +105,6 @@ let constListToString (c : const list) : string list =
     | String s -> s
   in
   List.map helper c
-
-let printToFile (inp : string list) (file_path : string) : unit = 
-  let fp = open_out file_path in
-  let lekunga = (String.concat "\n" inp)
-  in
-  let () = Printf.fprintf fp "%s" lekunga in
-    close_out fp
-(*When it comes to parsing src, you should find it useful that fold_left can be
-  defined and used with strings (like String.fold_left in new OCaml version).
-  See String.get. These are suggestions though and you are welcome
-  to use what you want :)  *)
 
 let error = (true, (String("\"Error\""))::[])
 
@@ -196,6 +210,7 @@ let concat (b : bool) (accum : const list) : (bool * (const list)) =
         in
         (b, (String("\"" ^ removeQuotes (i ^ j) ^ "\""))::a)
 
+
 let interpret (acc : (bool * (const list))) (comm : com) : (bool * (const list)) = 
   let (b, accum) = acc
   in
@@ -226,6 +241,161 @@ let interpret (acc : (bool * (const list))) (comm : com) : (bool * (const list))
     | Concat -> concat b accum
     | Neg -> neg b accum
     | _ -> acc
+
+
+
+
+let (@@) (f : 'a -> 'b) (a : 'a): 'b =
+  f a
+
+let (|>) (a : 'a) (f : 'a -> 'b): 'b =
+  f a
+
+  
+type env =
+ (string * const) list list
+
+type stack = const list
+
+
+type program = (stack * env)
+type ('a, 'e) result =
+  | Ok of 'a
+  | Err of 'e
+
+let ok  (res : 'a): ('a, 'e) result = Ok(res)
+let err (err : 'e): ('a, 'e) result = Err(err)
+let fold_result (f : 'a -> 'b) (g : 'e -> 'b) (res : ('a, 'e) result): 'b =
+  match res with
+  | Ok(o) -> f o
+  | Err(e) -> g e
+
+let and_then (f : 'a -> ('b, 'e) result) (res : ('a, 'e) result): ('b, 'e) result =
+  fold_result f err res
+
+type parse_error =
+  | DivideByZero
+  | UnmatchedBegin
+  | EmptyStack
+  | WrongDatatype
+  | Place
+
+let global (x : string) (p : program) : (program, parse_error) result =
+  let (stack, e) = p
+  in
+  let glob = List.hd e
+  in
+  if List.length stack <= 0
+    then
+      err(Place)
+  else
+    let value = List.hd stack
+    in
+    let tup = (x, value)
+    in
+    let newP = (List.tl stack, (tup::glob)::(List.tl e))
+    in
+    ok(newP)
+
+(*   
+let findVar (acc : option) (l : (string * const) list) = 
+  match acc with
+  | None -> List.assoc_opt x l
+  | _ -> acc
+in
+let v = List.fold_left findVar l None
+*)
+let push (h: const) (t :com list) (p : program) = 
+  let (stk, e) = p
+  in
+  match h with
+  | Var vr -> 
+    let findVar (acc : const option) (l : (string * const) list) = 
+    match acc with
+      | None -> List.assoc_opt vr l
+      | _ -> acc
+    in
+    let v = List.fold_left findVar None (List.rev e)
+    in
+    (match v with
+    | Some s -> ok(((s::stk,e),t))
+    | _ -> err(Place))
+  | _ -> ok(((h::stk,e),t))
+
+
+
+let local (x : string) (p : program) : (program, parse_error) result =
+  let (stack, e) = p
+  in
+  if List.length stack <= 0
+    then
+      err(Place)
+  else
+    let loc = List.hd (List.rev e)
+    in
+    let value = List.hd stack
+    in
+    let tup = (x, value)
+    in
+    let newP = (List.tl stack, (tup::loc)::(List.tl e))
+    in
+    ok(newP)
+(*
+let rec parse_sexpr (st : com list) (p : program) : (program * (com list) , parse_error) result = 
+    let h = List.hd st
+    in
+    let t = List.tl st
+    in
+    let (_,e) = p
+    in
+    match h with
+    | Quit -> err(Place)
+    | Begin -> parse_sexpr_list st ([],e) |> and_then @@ fun (newP, newT) -> 
+      ok((newP,newT))
+    | End -> err(Place)
+    | Push(x) -> push x t p
+    | Global(x) -> (global x p) |> and_then @@ fun newP -> ok((newP,t))
+    | Local(x) -> local x p |> and_then @@ fun newP -> ok((newP,t))
+    | _ -> err(Place)
+  and parse_sexpr_list (st : com list) (p : program) : (program * (com list), parse_error) result =
+      let h = List.hd st
+      in
+      match h with
+      | End -> ok((p,List.tl st))
+      | _ -> parse_sexpr st p
+         |> and_then @@ fun (fst, st') ->
+         parse_sexpr_list st'
+         |> and_then @@ fun (rst, st'') ->
+         ok (fst::rst, st'')
+
+let parse (st : com list): (const list, parse_error) result =
+  let rec parse_all st (p : program) =
+    match st with
+    | _ -> err(Place)
+    | h::t ->
+      match h with
+      | Quit -> 
+        let (stack, _) = p
+        in
+        ok @@ stack
+      | _   -> parse_sexpr t p
+                |> and_then @@ fun (newP, ts) ->
+              parse_all ts newP
+    in
+  parse_all st ([], [[]])
+
+*)
+
+
+
+
+
+
+
+
+
+
+
 
 let interpreter (src : string) (output_file_path: string): unit =
   let commList = srcToCom src
