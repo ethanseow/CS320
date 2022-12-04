2a6d02c32990be0ac45f23c4bb14d02b88ca9942
Ethan Seow <eseow6088@bths.edu>
2022-12-04 16:37:44 -0500

    added all commands for part 3

--- sandbox2.ml
+++ sandbox2.ml
@@ -8,12 +8,20 @@ type const =
   | String of string
   | Var of string
   | Clo of (string * string * com list)
+  | Left of const 
+  | Right of const
+  | TupleConst of (const list)
   and com = 
   | Quit
+  | Get of int
+  | Case of (com list * com list)
+  | Fun of (string * string * com list)
   | Push of const
+  | Tuple of int
   | Pop
   | Add
   | Sub
+  | Call
   | Mul
   | Div
   | Swap
@@ -29,7 +37,11 @@ type const =
   | Lte
   | IfThen of ifthen
   | Equal
+  | Return
+  | InjL
+  | InjR
   and ifthen = (com list * com list)
+type union 
 type ('a, 'e) result =
   | Ok of 'a
   | Err of 'e
@@ -42,9 +54,14 @@ type parse_err =
   | NotValidConst
   | NoEndStatementOnBegin
   | NoEndStatementOnIf
+  | NoEndStatementOnLR
+  | NoEndStatementOnFun
   | EndNotSkipped
   | ElseNotSkipped
   | Unmatched
+  | MissingReturnStatement
+  | MissingReturnStatementMut
+  | NotInClosure
 type result_out = ((com list) * program)
 let fold_result (f : 'a -> 'b) (g : 'e -> 'b) (res : ('a, 'e) result): 'b =
   match res with
@@ -59,7 +76,9 @@ let and_then (f : 'a -> ('b, 'e) result) (res : ('a, 'e) result): ('b, 'e) resul
 *)
 let regexp = Str.regexp "[-0-9]+$"
 let regexp2 = Str.regexp "\\\"[A-Za-z]+\\\"$"
-let regexp3 = Str.regexp "[a-z]+$"
+
+(* for variables - may need to change due to f being in front*)
+let regexp3 = Str.regexp "[a-z]+\\([0-9]|_\\)?$"
 let stringToConst (str : string ) : (const,parse_err) result =
   if Str.string_match regexp str 0
     then
@@ -85,21 +104,22 @@ let rec stringToCom (cmds : string list): ((com * string list), parse_err) resul
   let c = List.hd x
   in
   match List.length x with
-  | 2 ->
+  | 3 ->
     (
       if c = "Fun" || c = "Mut" then
-        let funcHeader = List.nth 1 x
+        let funcHeader = List.nth x 1
         in
+        let funcArg = List.nth x 2
         in
-        let funcArg = List.nth 2 x
-        in
-        let prog = find_fun t |> and_then @@ ( fun(coms, cmds) ->
-          let closure = Clo(funcHeader, funcArg, coms)
-          ok( (Clo(), cmds)) )
+        find_fun t [] |> and_then @@ ( fun(coms, cmds) ->
+          let closure = Fun(funcHeader, funcArg, coms)
+          in
+          ok( (closure, cmds)) )
+        
       else
         err(Unmatched)
     )
-  | 1 -> 
+  | 2 -> 
     (
       match stringToConst (List.nth x 1) with
       | Ok(n) -> 
@@ -108,16 +128,26 @@ let rec stringToCom (cmds : string list): ((com * string list), parse_err) resul
           | "Push" -> ok((Push(n),t))
           | "Global" -> ok((Global(n),t))
           | "Local" -> ok((Local(n),t))
+          | "Tuple" -> 
+            let Int i = n
+            in
+            ok( (Tuple(i),t) )
+          | "Get" ->
+            let Int i = n
+            in
+            ok( (Get(i),t) )
         )
       | Err(e) -> err(NotValidConst)
     )
-  | _ ->
+  | 1 ->
     (
     match c with
     | "Begin" -> (find_begin_end t []) |> and_then @@ fun (coms, cmds) ->
       ok( (Begin(coms), cmds) )
     | "IfThen" -> (find_ifthen_end t [] [])|> and_then @@ fun (branches, cmds) ->
       ok( (IfThen(branches), cmds) )
+    | "CaseLeft" -> (find_lr t [] [])|> and_then @@ fun (branches, cmds) ->
+      ok( (Case(branches), cmds) )
     | "Quit" -> ok((Quit,t))
     | "Pop" -> ok((Pop,t))
     | "Add" -> ok((Add,t))
@@ -129,11 +159,15 @@ let rec stringToCom (cmds : string list): ((com * string list), parse_err) resul
     | "Neg" -> ok((Neg,t))
     | "End" -> err(EndNotSkipped)
     | "Else" -> err(ElseNotSkipped)
+    | "Return" -> err(NotInClosure)
     | "And" -> ok((And,t))
+    | "InjL" -> ok((InjL,t))
+    | "InjR" -> ok((InjR,t))
     | "Or" -> ok((Or,t))
     | "Lte" -> ok((Lte,t))
     | "Not" -> ok((Not,t))
     | "Equal" -> ok((Equal,t))
+    | "Call" -> ok((Call,t))
     )
   and find_begin_end (cmds : string list) (acc : com list): ((com list * string list), parse_err) result = 
     match cmds with
@@ -167,7 +201,47 @@ let rec stringToCom (cmds : string list): ((com * string list), parse_err) resul
         (* first argument always gets populated *)
         find_ifthen_end rest (com::ift) els
   and find_fun (cmds : string list) (acc : com list) : ((com list * string list), parse_err) result = 
-      
+    match cmds with
+    | [] -> err(NoEndStatementOnFun)
+    | h::t -> 
+      let x = List.hd(String.split_on_char ' ' h)
+      in
+      match x with
+      | "Return" ->
+        find_fun t (Return::acc) 
+      | "End" -> 
+        if List.length acc = 0 || ((List.hd acc) <> Return) then
+          err(MissingReturnStatement)
+        else
+          ok( ( (List.rev acc), t) )
+
+      | "Mut" -> 
+        if List.hd acc <> Return then
+          err(MissingReturnStatementMut)
+        else
+          ok( ( (List.rev acc), cmds) )
+      | _ ->
+        stringToCom cmds |> and_then @@ fun (com,rest) ->
+        find_fun rest (com::acc) 
+  and find_lr (cmds : string list) (left : com list) (right : com list) : ((ifthen * string list), parse_err) result = 
+    match cmds with
+    | [] -> err(NoEndStatementOnLR)
+    | h::t -> 
+      let x = List.hd(String.split_on_char ' ' h)
+      in
+      match x with 
+      | "End" -> 
+        (* swapped because else swaps the two arguments*)
+        let branch : ifthen = ( (List.rev right) , (List.rev left ) )
+        in
+        ok( ( branch, t) )
+      | "Right" -> 
+        (* put populated if branch to the side and populate the els branch*)
+        find_ifthen_end t right left
+      | _ ->
+        stringToCom cmds |> and_then @@ fun (com,rest) ->
+        (* first argument always gets populated *)
+        find_lr rest (com::left) right
 let parse (src : string) : (com list,parse_err) result = 
   let cmds = String.split_on_char '\n' src
   in
@@ -180,96 +254,21 @@ let parse (src : string) : (com list,parse_err) result =
   in
   parse_all cmds []
 
-let a = "IfThen
-Begin
-End
-Else
-Begin
+let a = "Push 5
+InjL
+CaseLeft
 Push 3
-Begin
+CaseLeft
+Push 5
+Right
+Push 5
+End
 Push 4
-End
-End
-End
-Push 4
-Begin
-End
-Push 5
-Quit";;
-
-let b = "Push 23
-Local x
-Push x
-Push 24
-Global y
-Push y
-Sub
-IfThen
-Push 37
-Push 1
-Local x
-Push x
-Begin
-Push 52
-Local x
-Push 7
-Global y
-Push y
-End
-Else
-Push 32
-Push 5
-Global y
-Push x
-Begin
-Push 71
-Local y
-Push 8
-Global x
-Push x
-End
-End
-Push x
-Push y
+Right
 Add
-Quit";;
-
-let q = "Push 23
-Local x
-Push x
-Push 24
-Global y
-Push y
-Sub
-IfThen
-Push 37
-Push 1
-Local x
-Push x
-Begin
-Push 52
-Local x
-Push 7
-Global y
-Push y
 End
-Else
-Push 32
-Push 5
-Global y
-Push x
-Begin
-Push 71
-Local y
-Push 8
-Global x
-Push x
-End
-End
-Push x
-Push y
-Add
-Quit";;
-match parse a with
-| Ok(n) -> List.rev n
-| Err(e) -> []
+Tuple 3
+Get 2
+Quit
+Call";;
+parse a;;
